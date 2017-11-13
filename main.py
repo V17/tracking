8bb488ab758e157e95ba06eb2dc912c51c666a73
vozka <vojtav@centrum.cz>
2017-11-13 02:11:26 +0100

    nastrel objektu na ukladani bodu

--- main.py
+++ main.py
@@ -6,6 +6,37 @@ import matplotlib.pyplot as plt
 from itertools import izip_longest
 from math import sqrt
 
+class TipCoords:
+    def __init__(self, coords, timeframe):
+        self.coords_list = []
+        for i in range(timeframe):
+            self.coords_list.append((-1, -1, -1))
+        self.coords_list.append(coords)
+
+    def add_coordinate(self, coords):
+        self.coords_list.append(coords)
+
+    def get_last_coord(self):
+        return self.coords_list[len(self.coords_list)-1]
+
+
+class TipSet:
+    def __init__(self):
+        self.tip_list = []
+        self.timeframe = 0
+
+    def add_new_tip(self, coords):
+        self.tip_list.append(TipCoords(coords, self.timeframe))
+
+    def set_timeframe(self, timeframe):
+        self.timeframe = timeframe
+
+    def add_coords_to_tip(self, old_coords, new_coords):
+        for tip in self.tip_list:
+            if old_coords == tip.get_last_coord():
+                tip.add_coordinate(new_coords)
+
+
 
 def read_points(filename, sizex, sizey):
     """
@@ -52,44 +83,34 @@ def get_offset_from_xyz(xyz, sizex, sizey):
         return z * sizex * sizey + y * sizex + x
 
 
-def build_bipartite_graph(points1, points2):
+def build_bipartite_graph(points1, points2, maxdist=0.000001):
     """
     Builds a bipartite graph from 2 sets of points and writes the euclidean distance into the edge weights and point
     coordinates into the nodes
     :return: Graph object containing the above
     """
-    """points1 = []
-    points2 = []
-    for point in range(len(points1)):
-        if points1_full[point] != (-1, -1, -1) or points2_full[point] != (-1, -1, -1):
-            points1.append(points1_full[point])
-            points2.append(points2_full[point])"""
 
     points1 = [x for x in points1 if x != (-1, -1, -1)]
     points2 = [x for x in points2 if x != (-1, -1, -1)]
     size1 = len(points1)
     size2 = len(points2)
-    size = max(size1, size2)
-    if size1 < size2:
-        for i in range(size2-size1):
-            points1.append((-1, -1, -1))
-    if size1 > size2:
-        for i in range(size1-size2):
-            points2.append((-1, -1, -1))
+    for i in range(len(points1)):
+        points2.append((-1, -1, -1))
+
     print "matching", len(points1)+len(points2), "points"
     print "set 1:", points1
     print "set 2:", points2
-    G = nx.complete_bipartite_graph(size, size)
+    G = nx.complete_bipartite_graph(size1, size2)
 
-    for i in range(0, size):
-        for j in range(0, size):
+    for i in range(0, size1):
+        for j in range(0, size2):
             if points1[i] != (-1, -1, -1) and points2[j] != (-1, -1, -1):
                 dist = anisotropic_euclid_distance(points1[i], points2[j])
             else:
-                dist = 0.0000000001
-            G.edge[i][j + size]['weight'] = dist
+                dist = maxdist
+            G.edge[i][j + size1]['weight'] = dist
             G.node[i]['coordinates'] = points1[i]
-            G.node[j+size]['coordinates'] = points2[j]
+            G.node[j+size1]['coordinates'] = points2[j]
     return G
 
 
@@ -126,19 +147,23 @@ def check_points(coord_pairs, points, frame):
     """
     true_matches = 0
     false_matches = 0
+    unmatched = 0
     for pair in coord_pairs:
         coord1 = pair[0]
         coord2 = pair[1]
         point_index = points[frame].index(coord1)
-        if points[frame+1][point_index] == coord2:
-            true_matches += 1
-            print "matched point", get_offset_from_xyz(coord1, sizex, sizey), coord1, "with", get_offset_from_xyz(coord2, sizex, sizey), coord2, "correctly"
+        if coord1 == (-1, -1, -1) or coord2 == (-1, -1, -1):
+            unmatched += 1
         else:
-            false_matches += 1
-            print "matched point", get_offset_from_xyz(coord1, sizex, sizey), coord1, "with", get_offset_from_xyz(coord2, sizex, sizey), coord2, "incorrectly, correct match was", get_offset_from_xyz(points[frame+1][point_index], sizex, sizey), points[frame+1][point_index]
-    print "true matches:", true_matches, ", false matches:", false_matches
+            if points[frame+1][point_index] == coord2:
+                true_matches += 1
+                print "matched point", get_offset_from_xyz(coord1, sizex, sizey), coord1, "with", get_offset_from_xyz(coord2, sizex, sizey), coord2, "correctly"
+            else:
+                false_matches += 1
+                print "matched point", get_offset_from_xyz(coord1, sizex, sizey), coord1, "with", get_offset_from_xyz(coord2, sizex, sizey), coord2, "incorrectly, correct match was", get_offset_from_xyz(points[frame+1][point_index], sizex, sizey), points[frame+1][point_index]
+    print "true matches:", true_matches, ", false matches:", false_matches, "unmatched:", unmatched
     print "---"
-    return true_matches, false_matches
+    return true_matches, false_matches, unmatched
 
 
 filename = "./tip_tracking/ID428/tip_trajectories_pxOffsets.txt"
@@ -147,6 +172,7 @@ sizey = 261
 points_list = read_points(filename, sizex, sizey)
 total_true_matches = 0
 total_false_matches = 0
+total_unmatched = 0
 for i in range(len(points_list)-1):
     print "matching frames", i, ",", i+1
     b_g = build_bipartite_graph(points_list[i], points_list[i+1])
@@ -154,6 +180,7 @@ for i in range(len(points_list)-1):
     matches = check_points(pairs, points_list, i)
     total_true_matches += matches[0]
     total_false_matches += matches[1]
+    total_unmatched += matches[2]
 
-print "Matched", total_true_matches, "correctly and", total_false_matches, "incorrectly in total."
+print "Matched", total_true_matches, "correctly and", total_false_matches, "incorrectly in total, ", total_unmatched, "pairs were not matched."
 
