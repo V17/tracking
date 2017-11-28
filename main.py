960ea57c2caa5334e86e46366b9193dcefaf537a
vozka <vojtav@centrum.cz>
2017-11-28 01:34:56 +0100

    refactoring.

--- main.py
+++ main.py
@@ -6,106 +6,85 @@ from itertools import izip_longest
 from math import sqrt
 
 
-class TipCoords:
-    def __init__(self, coords, timeframe):
-        self.coords_list = []
-        for i in range(timeframe):
-            self.coords_list.append((-1, -1, -1))
-        self.coords_list.append(coords)
-
-    def add_coordinate(self, coords):
-        self.coords_list.append(coords)
-        if coords != self.coords_list[len(self.coords_list)-1]:
-            print "error adding point", coords, "should be", self.coords_list[len(self.coords_list)-1]
-
-    def get_last_coord(self, framedif):
-        if len(self.coords_list) <= framedif:
-            framedif = len(self.coords_list)
-        for i in range(framedif):
-            if self.coords_list[len(self.coords_list)-i-1] != (-1, -1, -1):
-                return self.coords_list[len(self.coords_list)-i-1]
-        return (-1, -1, -1)
-
-    def get_coords(self):
-        return self.coords_list
-
-    def get_match(self, timeframe):
-        if timeframe > len(self.coords_list)-1:
-            print "match not yet made"
-        else:
-            return (self.coords_list[timeframe-1], self.coords_list[timeframe])
-
-
 class TipSet:
-    def __init__(self, sizex, sizey):
-        self.tip_list = []
+    def __init__(self, sizex, sizey, coordlist, maxdist=0.05):
+        """after initialization the timeframe is set to 0, the list of frames is filled and match_list contains
+        a 1-long list of 1 coordinate for each point in the first frame"""
+        self.frame_list = coordlist
+        self.match_list = []
         self.timeframe = 0
         self.sizex = sizex
         self.sizey = sizey
+        self.maxdist = maxdist
+        for coord in self.frame_list[0]:
+            if coord != (-1, -1, -1):
+                self.match_list.append([coord])
 
-    def add_new_tip(self, coords):
-        self.tip_list.append(TipCoords(coords, self.timeframe))
+    def get_max_bipartite_graph(self):
+        # Initialize lists of existing points and new points, add dummy points into new points list
+        old_points = self.get_last_frame()
+        new_points = []
+        for coord in self.frame_list[self.timeframe+1]:
+            if coord != (-1, -1, -1):
+                new_points.append(coord)
+        for i in range(len(old_points)):
+            new_points.append((-1, -1, -1))
+        print "old points", old_points
+        print "new points", new_points
 
-    def set_timeframe(self, timeframe):
-        self.timeframe = timeframe
+        # Build a bipartite graph, set weights to inverse of euclidean distance
+        G = nx.complete_bipartite_graph(len(old_points), len(new_points))
+        for i in range(0, len(old_points)):
+            for j in range(0, len(new_points)):
+                if new_points[j] != (-1, -1, -1):
+                    dist = anisotropic_euclid_distance(old_points[i], new_points[j])
+                else:
+                    dist = self.maxdist - 0.0001
+                G.edge[i][j + len(old_points)]['weight'] = dist
+                G.node[i]['coordinates'] = old_points[i]
+                G.node[j + len(old_points)]['coordinates'] = new_points[j]
 
-    def get_timeframe(self):
-        return self.timeframe
+        return G
 
-    def increment_timeframe(self):
-        self.timeframe += 1
-        for tip in self.tip_list:
-            if len(tip.get_coords()) < self.timeframe:
-                tip.add_coordinate((-1, -1, -1))
-
-    def correct_timeframe(self):  # toto je bastl ktery by nemel byt potreba, ale...
-        for tip in self.tip_list:
-            if len(tip.get_coords()) < self.timeframe+1:
-                tip.add_coordinate((-1, -1, -1))
-
-    def add_coords_to_tip(self, old_coords, new_coords, framedif=1):
-        for tip in self.tip_list:
-            if old_coords == tip.get_last_coord(framedif):
-                tip.add_coordinate(new_coords)
-                return 1
-        for tip in self.tip_list:
-            if new_coords == tip.get_last_coord(framedif):
-                tip.add_coordinate(old_coords)
-                return 1
-        print "error: tip that should have existed not found at coords", get_offset_from_xyz(old_coords, self.sizex, self.sizey), "and frame", self.timeframe
-        return 0
-
-    def check_if_exists(self, coords, framedif=1):
-        for tip in self.tip_list:
-            if coords == tip.get_last_coord(framedif):
-                return True
-        return False
-
-    def get_last_frame(self, framedif=1):
-        coordlist = []
-        for tip in self.tip_list:
-            coord = tip.get_last_coord(framedif)
-            if coord is not None:
-                coordlist.append(tip.get_last_coord(framedif))
-        return coordlist
-
-    def get_all_points(self):
-        for tip in self.tip_list:
-            offsets = []
-            for coord in tip.get_coords():
-                offsets.append(get_offset_from_xyz(coord, self.sizex, self.sizey))
-            print offsets
-
-    def get_matches_for_frame(self, timeframe):
-        matches = []
-        real_frame = len(self.tip_list[0].get_coords())-1
-        if timeframe > real_frame or self.timeframe < 2:
-            print "no matches made yet for timeframe", timeframe, "current timeframe:", self.timeframe
-            return []
-        for tip in self.tip_list:
-            matches.append(tip.get_match(timeframe))
-        # print "successfuly returning matches for timeframes", timeframe, "and", timeframe-1
+    def get_max_weight_matches(self, graph):
+        # Compute the max weight matching and remove redundant edges (existing edges with inverse orientation)
+        H = nx.max_weight_matching(graph)
+        for i in range(len(H)/2, len(H)):
+            del H[i]
+        matches = {}
+        for edge in H:
+            matches[graph.node[edge]['coordinates']] = graph.node[H[edge]]['coordinates']
+        print matches
         return matches
+
+    def add_matches(self, matches_dict):
+        # add matched pairs and vanishing points into match_list, find and add new points, increase the timeframe
+        new_points = []
+        for coord in self.frame_list[self.timeframe+1]:
+            if coord != (-1, -1, -1):
+                new_points.append(coord)
+        for match in self.match_list:
+            if match[self.timeframe] != (-1, -1, -1):
+                match.append(matches_dict[match[self.timeframe]])
+                try:
+                    if matches_dict[match[self.timeframe]] != (-1, -1, -1):
+                        new_points.remove(matches_dict[match[self.timeframe]])
+                except ValueError:
+                    print "coord not in new_points list:", matches_dict[match[self.timeframe]]
+        for new_point in new_points:
+            temp_coords = []
+            for i in range(self.timeframe+1):
+                temp_coords.append((-1, -1, -1))
+            temp_coords.append(new_point)
+            self.match_list.append(temp_coords)
+        self.timeframe +=1
+
+    def get_last_frame(self, framedif=1):  # TODO: implement looking back
+        coordlist = []
+        for coords in self.match_list:
+            if coords[self.timeframe] != (-1, -1, -1):
+                coordlist.append(coords[self.timeframe])
+        return coordlist
 
 
 def read_points(filename, sizex, sizey):
@@ -152,75 +131,6 @@ def get_offset_from_xyz(xyz, sizex, sizey):
         return z * sizex * sizey + y * sizex + x
 
 
-def build_bipartite_graph2(matched_points, new_points, maxdist=0.05, framedif=1):
-    old_points = matched_points.get_last_frame(framedif)
-    new_points = [x for x in new_points if x != (-1, -1, -1)]
-    size1 = len(old_points)
-    size2 = len(new_points)
-    if size1 < size2:
-        for i in range(size2-size1):
-            old_points.append((-1, -1, -1))
-    for i in range(size1):
-        new_points.append((-1, -1, -1))
-    G = nx.complete_bipartite_graph(len(old_points), len(new_points))
-
-    for i in range(0, len(old_points)):
-        for j in range(0, len(new_points)):
-            if new_points[j] != (-1, -1, -1) and old_points[i] != (-1, -1, -1):
-                dist = anisotropic_euclid_distance(old_points[i], new_points[j])
-            elif new_points[j] == (-1, -1, -1) and old_points[i] == (-1, -1, -1):
-                dist = 0.00000
-            else:
-                dist = maxdist-0.01
-            G.edge[i][j + len(old_points)]['weight'] = dist
-            G.node[i]['coordinates'] = old_points[i]
-            G.node[j + len(old_points)]['coordinates'] = new_points[j]
-    return G
-
-    
-def build_bipartite_graph(points1, points2, maxdist=0.05):
-    """
-    Builds a bipartite graph from 2 sets of points and writes the euclidean distance into the edge weights and point
-    coordinates into the nodes
-    :return: Graph object containing the above
-    """
-
-    points1 = [x for x in points1 if x != (-1, -1, -1)]
-    points2 = [x for x in points2 if x != (-1, -1, -1)]
-    size1 = len(points1)
-    for i in range(len(points1)):
-        points2.append((-1, -1, -1))
-    size2 = len(points2)
-
-    print "matching", len(points1)+len(points2), "points"
-    print "set 1:", points1
-    print "set 2:", points2
-    G = nx.complete_bipartite_graph(size1, size2)
-
-    for i in range(0, size1):
-        for j in range(0, size2):
-            if points2[j] != (-1, -1, -1):
-                dist = anisotropic_euclid_distance(points1[i], points2[j])
-            else:
-                dist = maxdist
-            G.edge[i][j + size1]['weight'] = dist
-            G.node[i]['coordinates'] = points1[i]
-            G.node[j+size1]['coordinates'] = points2[j]
-    return G
-
-
-def find_max_match(bipartite_graph):
-    """
-    Computes the maximum-weight matching of a bipartite graph and returns the result in the form of coordinate pairs
-    :return: list of tuples containing matched coordinate pairs
-    """
-    H = nx.max_weight_matching(bipartite_graph)
-    coord_pairs = []
-    for edge in H:
-        coord_pairs.append((bipartite_graph.node[edge]['coordinates'], bipartite_graph.node[H[edge]]['coordinates']))
-    return coord_pairs[:len(coord_pairs)/2]
-
-
 def anisotropic_euclid_distance(point1, point2, zratio=0.125):
     xa, ya, za = point1
     xb, yb, zb = point2
@@ -232,30 +142,6 @@ def anisotropic_euclid_distance(point1, point2, zratio=0.125):
         dist = 9999999999                                              # Tohle mozna neni tak uplne nejlepsi napad
         # print "dist is 0, point coords are", point1, point2
     return dist
-
-
-def add_matched_pairs(coord_pairs, matched_points):
-    matched_points.increment_timeframe()
-    for pair in coord_pairs:
-        coord1 = pair[0]
-        coord2 = pair[1]
-        if coord1 != (-1, -1, -1) and coord2 != (-1, -1, -1):
-            if matched_points.add_coords_to_tip(coord1, coord2) == 1:
-                continue
-            else:
-                print "error with coords", coord1, coord2
-
-        elif coord2 != (-1, -1, -1):
-            if not matched_points.check_if_exists(coord2):
-                matched_points.add_new_tip(coord2)
-            #print "added new tip in coordinates", coord2
-        elif coord1 != (-1, -1, -1):
-            if not matched_points.check_if_exists(coord1):
-                matched_points.add_new_tip(coord1)
-        else:
-            continue
-            #print "match not added", coord1, coord2
-    matched_points.correct_timeframe()
 
 
 def check_points(coord_pairs, points, frame, sizex, sizey):
@@ -332,60 +218,14 @@ def check_correct_matches(detected_tipset, control_tipset):
     print "Total score:", correct_matches, "matched correctly,", wrong_matches, "matched incorrectly,", incorrectly_found, "incorrectly labeled as new,", incorrectly_vanished, "incorrectly labeled as vanished"
 
 
-def build_control_tipset(control_points, sizex, sizey):
-    control_tipset = TipSet(sizex, sizey)
-    for coord in range(len(control_points[0])):
-        temptip = TipCoords(control_points[0][coord], 0)
-        for frame in range(1, len(control_points)):
-            temptip.add_coordinate(control_points[frame][coord])
-        control_tipset.tip_list.append(temptip)
-    control_tipset.set_timeframe(len(control_points[0]))
-    return control_tipset
-
-
 def apply_and_check(filename, sizex, sizey, frames=0):
     points_list = read_points(filename, sizex, sizey)
-    tipset = TipSet(sizex, sizey)
-    for coordinate in points_list[0]:
-        if coordinate != (-1, -1, -1):
-            tipset.add_new_tip(coordinate)
-    control = build_control_tipset(points_list, sizex, sizey)
-    if frames == 0:
-        frames = len(points_list) - 1
-        print "checking", frames + 1, "frames"
-    for i in range(frames):
-        b_g = build_bipartite_graph2(tipset, points_list[i + 1], 0.05)
-        pairs = find_max_match(b_g)
-        add_matched_pairs(pairs, tipset)
-    # tipset.get_all_points()
-    # control.get_all_points()
-    tipset.correct_timeframe()
-    print ""
-    check_correct_matches(tipset, control)
+    tipset = TipSet(sizex, sizey, points_list)
+    G = tipset.get_max_bipartite_graph()
+    matches = tipset.get_max_weight_matches(G)
+    tipset.add_matches(matches)
+    print tipset.match_list
 
 
 apply_and_check("./tip_tracking/ID319/tip_trajectories_pxOffsets.txt", 159, 146, 0)
-
-'''
-filename = "./tip_tracking/ID319/tip_trajectories_pxOffsets.txt"
-sizex = 159
-sizey = 146
-points_list = read_points(filename, sizex, sizey)
-
-tipset = TipSet()
-for coordinate in points_list[0]:
-    if coordinate != (-1, -1, -1):
-        tipset.add_new_tip(coordinate)
-
-control = build_control_tipset(points_list)
-
-for i in range(len(points_list[0])-1):
-    b_g = build_bipartite_graph2(tipset, points_list[i+1])
-    pairs = find_max_match(b_g)
-    add_matched_pairs(pairs, tipset)
-    #tipset.get_all_points()
-    print "-----"
-check_correct_matches(tipset, control)
-'''
-
 
