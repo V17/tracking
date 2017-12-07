7f6ad0e4870bf1e1ccf8c5c1f095be2a81e4ff77
vozka <vojtav@centrum.cz>
2017-12-07 04:51:41 +0100

    correctness checking fixed *again*, this time seems actually correct? implemented f1 score checking

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import networkx as nx
 from itertools import izip_longest
 from math import sqrt
 from operator import add
+import time
 
 
 class TipSet:
@@ -89,8 +90,19 @@ class TipSet:
     def get_last_location(self, match, framedif):
         result = (-1, -1, -1)
         for i in range(framedif):
-            if match[self.timeframe-i] != (-1, -1, -1):
-                return match[self.timeframe-i]
+            if match[self.timeframe - i] != (-1, -1, -1):
+                return match[self.timeframe - i]
+        return result
+
+    def get_last_location_check(self, match, framedif, index=-1):
+        result = (-1, -1, -1)
+        if index == -1:
+            index = self.timeframe
+        if framedif == -1:
+            framedif = index + 1
+        for i in range(0, framedif):
+            if i <= index and match[index-i] != (-1, -1, -1):
+                return match[index-i]
         return result
 
     def get_last_frame(self, framedif=1):
@@ -100,6 +112,12 @@ class TipSet:
                 if coords[self.timeframe-i] != (-1, -1, -1):
                     coordlist.append(coords[self.timeframe-i])
                     break
+        return coordlist
+
+    def get_complete_frame(self, frame):
+        coordlist = []
+        for coords in self.match_list:
+            coordlist.append(coords[frame])
         return coordlist
 
     def get_pairs_for_frame(self, timeframe):
@@ -142,6 +160,10 @@ def build_control_tipset(coord_list, sizex, sizey):
     return control
 
 
+def get_point_index(pointset, point, frame):
+    pass
+
+
 def check_found_and_control(found, control, frame, framedif):  # funguje to, ale je potreba divat se o x kroku zpatky... uf.
     found_pairs = found.get_pairs_for_frame(frame)
     control_pairs = control.get_pairs_for_frame(frame)
@@ -158,15 +180,19 @@ def check_found_and_control(found, control, frame, framedif):  # funguje to, ale
             else:
                 incorrectly_matched += 1
         elif pair[0] == (-1, -1, -1) and pair[1] != (-1, -1, -1):  # tady by se melo checkovat o par frejmu zpatky
-            matches_index = found_pairs.index(pair)
-            last_coord = found.get_last_location(found.match_list[matches_index], framedif)
+            matches_index_found = found.get_complete_frame(frame).index(pair[1])
+            matches_index_ctrl = control.get_complete_frame(frame).index(pair[1])
+            # print matches_index_found, matches_index_ctrl
+            # print "gonna look for point", get_offset_from_xyz(pair[1], 220, 220)
+            last_coord = found.get_last_location_check(found.match_list[matches_index_found], -1, frame-1)
+            last_real_coord = control.get_last_location_check(control.match_list[matches_index_ctrl], -1, frame - 1)
             if last_coord == (-1, -1, -1):
-                if pair in control_pairs:
+                if last_real_coord == (-1, -1, -1):
                     correctly_found += 1
                 else:
                     incorrectly_found += 1
+                    # print "incorrectly found pair", get_offset_from_xyz(pair[0], 220, 220), get_offset_from_xyz(pair[1], 220, 220)
             else:
-                last_real_coord = control.get_last_location(control.match_list[matches_index], framedif)
                 if last_coord == last_real_coord:
                     correctly_matched += 1
                 else:
@@ -223,12 +249,15 @@ def convert_match_to_offset(match, sizex, sizey):
     return get_offset_from_xyz(match[0], sizex, sizey), get_offset_from_xyz(match[1], sizex, sizey)
 
 
-def apply_and_check(filename, sizex, sizey, frames=0, framedif=1):
+def apply_and_check(filename, sizex, sizey, xypxsize=125, zratio=0.125, framedif=1, maxdistnm=6250, logging=True):
     points_list = read_points(filename, sizex, sizey)
-    if len(points_list) == 0:
-        print "empty list"
+    if len(points_list) == 0 and logging:
+        logfile = open(filename + "." + str(int(time.time())) + ".log", "w")
+        logfile.write("empty list")
+        logfile.close()
         return 0
-    tipset = TipSet(sizex, sizey, points_list, 0.01)  # s moc malou hranici pada, proc?
+    maxdistpx = 1.0/(maxdistnm/xypxsize)
+    tipset = TipSet(sizex, sizey, points_list, maxdistpx)  # s moc malou hranici pada, proc?
     for i in range(len(points_list)-1):
         G = tipset.get_max_bipartite_graph(framedif)
         matches = tipset.get_max_weight_matches(G)
@@ -240,14 +269,74 @@ def apply_and_check(filename, sizex, sizey, frames=0, framedif=1):
     for frame in range(1, len(points_list)):
         tempresults = check_found_and_control(tipset, control, frame, framedif)
         results = map(add, results, tempresults)
-    print "Checking file", filename, "maximum time gap between two tips is", framedif, "frames"
-    print "correctly matched:", results[0], "incorrectly matched:", results[1]
-    print "correctly recognized as new:", results[4], "incorrectly recognized as new:", results[5]
-    print "correctly recognized as vanishing:", results[2], "incorrectly recognized as vanishing:", results[3]
-    print "-------------------"
-    for point in tipset.match_list:
-        print [get_offset_from_xyz(item, sizex, sizey) for item in point]
+
+    precision = float(results[0])/(results[0]+results[1])
+    recall = float(results[0])/(results[3]+results[0]+results[5])
+    f1score = (2*precision*recall)/(precision+recall)
+    if logging:
+        logfile = open(filename + "." + str(int(time.time())) + ".log", "w")
+        logfile.write(
+            "Checking file " + filename + " maximum time gap between two tips is " + str(framedif-1) + " frames, " +
+            "maximum distance between two points is " + str(maxdistnm) + "nm. \n")
+        logfile.write("correctly matched: " + str(results[0]) + " incorrectly matched: " + str(results[1]) + "\n")
+        logfile.write(
+            "correctly recognized as new: " + str(results[4]) + " incorrectly recognized as new: " + str(
+                results[5]) + "\n")
+        logfile.write(
+            "correctly recognized as vanishing: " + str(results[2]) + " incorrectly recognized as vanishing: " + str(
+                results[3]) + "\n")
+        logfile.write("precision: "+str(precision)+", recall: "+str(recall)+", F1 score: "+str(f1score)+"\n")
+        logfile.write("-------------------" + "\n")
+
+        for point in tipset.match_list:
+            logfile.write(str([get_offset_from_xyz(item, sizex, sizey) for item in point])+"\n")
+        logfile.close()
+    return f1score
 
 
-apply_and_check("./real_images/real-phosphodefective-03.txt", 400, 270, 0, 2)
+filename = "./real_images/real-control-01.txt"
+size_x, size_y = 220, 220
+
+results = [[0]+[125*(2**x) for x in range(0,10)]]
+for framegap in range(1, 10):
+    temp = [framegap-1]
+    for dist in [125*(2**x) for x in range(0, 10)]:
+        #print "logging for framedif", framegap, "and distance", dist, "nm"
+        f1score = apply_and_check(filename, size_x, size_y, framedif=framegap, maxdistnm=dist, logging=False)
+        temp.append(f1score)
+    results.append(temp)
+
+
+def print_results(results):
+    print "{0:11s}".format("")+" | ",
+    for i in range(1, len(results[0])):
+        print "{0:5d}".format(results[0][i])+" nm | ",
+    print ""
+    for line in range(1, len(results)):
+        for i in range(len(results[line])):
+            if i == 0:
+                print "{0:4d}".format(results[line][i])+" frames | ",
+            else:
+                print "{0:8.4f}".format(results[line][i])+" | ",
+        print ""
+
+
+def write_results(results, filename):
+    result_file = open(filename+"-result.txt", 'w')
+    result_file.write("{0:11s}".format("")+" | ")
+    for i in range(1, len(results[0])):
+        result_file.write("{0:5d}".format(results[0][i])+" nm | ")
+    result_file.write("\n")
+    for line in range(1, len(results)):
+        for i in range(len(results[line])):
+            if i == 0:
+                result_file.write("{0:4d}".format(results[line][i])+" frames | ")
+            else:
+                result_file.write("{0:8.4f}".format(results[line][i])+" | ")
+        result_file.write("\n")
+    result_file.close()
+
+print_results(results)
+write_results(results, filename)
+# print apply_and_check("./real_images/real-control-01.txt", 220, 220, framedif=2, maxdistnm=16000, logging=True)
 
